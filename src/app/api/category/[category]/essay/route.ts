951dfa57b1f7750454e4d6f515ede501f60fd01e
Rony Moura <roniely@intech.com.br>
2025-10-14 00:16:50 -0300

    Fixing image upload

--- src/app/api/category/[category]/essay/route.ts
+++ src/app/api/category/[category]/essay/route.ts
@@ -3,15 +3,14 @@ import { slugify } from "@/lib/util";
 import { NextRequest, NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
 
-interface EssayApiParams {
+interface EssayPostParams {
   params: Promise<{
     category: string;
-    essayId: string;
   }>;
 }
 
-export async function POST(request: NextRequest, { params }: EssayApiParams) {
-  const { category: categorySlug, essayId } = await params;
+export async function POST(request: NextRequest, { params }: EssayPostParams) {
+  const { category: categorySlug } = await params;
 
   try {
     const essay = await request.json();
